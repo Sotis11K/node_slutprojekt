173b63c6b8f754f7de1279f07e27775323f8e3bd
Sotis11K <brian20041211@gmail.com>
2023-05-09 10:38:08 +0200

    making functions work

--- src/functionPlot.js
+++ src/functionPlot.js
@@ -5,6 +5,7 @@ const config = { }
 const math = create(all, config)
 
 
+
 var y = 'x^2'
 
 var x1i = ''
@@ -37,7 +38,7 @@ var options={
 };*/
 
 window.onload = (event) => {
-        [document.querySelector(".submit"), document.querySelector("#tangent-activate"), document.querySelector("#updateVec")].forEach(element=>{
+        [document.querySelector(".submit"), document.querySelector("#tangent-activate"), document.querySelector("#updateVec"), document.querySelector(".xVal-update"), document.querySelector(".deriv-update")].forEach(element=>{
             element.addEventListener("click", inputReader)
         })
         functionPlot(options)
@@ -60,7 +61,7 @@ function inputReader(){
                 width: 700,
                 height: 500,
                 xAxis: { domain: [-5, 5] },
-                yAxis: { domain: [-5, 5] },
+                yAxis: { domain: [-4, 6] },
                 grid: true,
                 data: [
                     {
@@ -72,6 +73,43 @@ function inputReader(){
             else if(mathOption == "y"){
                 console.log(mathOption)
                 var y = document.querySelector('#graphFunction').value;
+                var xVal = document.querySelector('.x-value').value;
+
+
+               
+
+
+
+                console.log(math.simplify(y).evaluate({x:xVal}))
+                var fx = (math.simplify(y).evaluate({x:xVal})).toString()
+
+                if(xDerValue == null || xDerValue == ""){
+                    var p = document.createElement("p")
+                    p.innerHTML = "f(x) = "+y
+                    document.querySelector(".deriv-output").append(p)
+                }
+
+
+                else if(xVal != null || xVal != ""){
+                    var p = document.createElement('p')
+                    p.innerHTML = "f("+xVal+") = "+fx
+                    document.querySelector(".xVal-output").append(p)
+                }
+
+                document.querySelector(".xVal-output").innerHTML = "f("+xVal+") = "+fx
+                
+
+
+                var simplifyX = document.querySelector(".simplify-string").value
+               
+
+
+                if(simplifyX != null || simplifyX != ""){
+                    var p2 = document.createElement("p")
+                    p.innerHTML = (math.simplify(simplifyX)).toString()
+                    document.querySelector("simplify-container").append(p)
+                }
+
                 options={
                     target: '#root',
                     title: 'Graf',
@@ -85,8 +123,39 @@ function inputReader(){
                             fn: y, color: 'blue'
                         }]
                 }
+                
             }
-                        
+                   
+            else if(mathOption == "integral"){
+
+                console.log(mathOption)
+        
+
+                var y = document.querySelector('#graphFunction').value;
+
+
+                options={
+                    target: '#root',
+                    title: 'Graf',
+                    width: 700,
+                    height: 500,
+                    xAxis: { domain: [-5, 5] },
+                    yAxis: { domain: [-5, 5] },
+                    grid: true,
+                    data: [
+                        {
+                            fn: y, color: 'blue',
+                            range: [x1, x2],
+                            closed: true
+                        }]
+                }
+                
+            }
+
+
+
+
+
 
 
             else if(mathOption == "integral"){
@@ -309,6 +378,45 @@ function inputReader(){
                         }]
                     }
                 }
+            }
+
+            else if(mathOption == "derivative"){
+                console.log(mathOption)
+                    var y = document.querySelector('#graphFunction').value;
+                    var deriv = math.derivative(y, 'x').toString()
+                    var xDerValue = document.querySelector(".x-deriv").value
+
+                    var p1 = document.createElement("p")
+                    p1.innerHTML = deriv
+                    document.querySelector(".deriv-output").append(p1)
+
+                   
+
+                    if(xDerValue != null || xDerValue != ""){
+                        var p3 = document.createElement("p")
+                        p3.innerHTML = (math.derivative(y, 'x').evaluate({x:xDerValue})).toString()
+                        document.querySelector(".deriv-output").append(p3)
+                    }
+                    
+    
+                    options={
+                        target: '#root',
+                        title: 'Graf',
+                        width: 700,
+                        height: 500,
+                        xAxis: { domain: [-5, 5] },
+                        yAxis: { domain: [-5, 5] },
+                        grid: true,
+                        data: [
+                            {
+                                fn: y, color: 'blue',
+                                derivative:{
+    
+                                    fn: deriv, color: 'blue',
+                                    updateOnMouseMove: true
+                                }
+                            }]
+                        }
             }
 
 
